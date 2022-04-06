e4b0c873610bef4b2832ac3113b7c0d00deab0b4
Gagan Deep <pandafy.dev@gmail.com>
2022-04-07 00:38:42 +0530

    [features] Added functionality to ignore errors
    Added documentation

--- ow_sentry_utils/__init__.py
+++ ow_sentry_utils/__init__.py
@@ -1,3 +1,14 @@
+import re
+
+
+SENTRY_IGNORE_ERRORS = {
+    'celery.worker.request': [r'Worker exited prematurely: signal 15 (SIGTERM) Job:'],
+    'multiprocessing': [
+        r'Process \"ForkPoolWorker-2\" pid:([\d.]+) exited with "signal 9 \(SIGKILL\)\"'
+    ],
+}
+
+
 def traces_sampler(context):
     if context['transaction_context']['op'] == 'celery.task':
         return 0.0
@@ -6,3 +17,13 @@ def traces_sampler(context):
     ):
         return 0.0001
     return 0.001
+
+
+def before_send(event, hint):
+    event_logger = event.get('logger', None)
+    if event_logger not in SENTRY_IGNORE_ERRORS.keys():
+        return event
+    error_message = event.get('logentry', {}).get('message', '')
+    for error in SENTRY_IGNORE_ERRORS[event_logger]:
+        if re.search(error, error_message):
+            return None
