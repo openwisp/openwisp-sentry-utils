8f8f550411b9c998aee977482ea5c9de207bce84
Gagan Deep <pandafy.dev@gmail.com>
2022-04-19 18:55:37 +0530

    [fix] Fixed before_send utility function
    - Fixed before_send ignoring all events from a logger
      "before_send" was ignoring all events from a logger that was mentioned
      in SENTRY_IGNORE_ERRORS.
    
    - Fixed regular expressions for celery error
    
    - Added comment and another ignored error

--- ow_sentry_utils/__init__.py
+++ ow_sentry_utils/__init__.py
@@ -1,11 +1,21 @@
 import re
 
-
 SENTRY_IGNORE_ERRORS = {
-    'celery.worker.request': [r'Worker exited prematurely: signal 15 (SIGTERM) Job:'],
-    'multiprocessing': [
-        r'Process \"ForkPoolWorker-2\" pid:([\d.]+) exited with "signal 9 \(SIGKILL\)\"'
+    # The celery autoscaler generates following errors while
+    # downscaling workers. The error is generated
+    # by the worker that gets terminated due to downscaling.
+    # Hence, it is safe to ignore these errors.
+    'celery.worker.request': [
+        r'Worker exited prematurely: signal 15 \(SIGTERM\) Job:',
+        r'Worker exited prematurely: signal 9 \(SIGKILL\) Job:',
     ],
+    'multiprocessing': [
+        (
+            r'Process \"ForkPoolWorker-([\d.]+)\" pid:([\d.]+)'
+            r' exited with "signal 9 \(SIGKILL\)\"'
+        )
+    ],
+    'celery.concurrency.asynpool': [r'Timed out waiting for UP message from '],
 }
 
 
@@ -27,3 +37,4 @@ def before_send(event, hint):
     for error in SENTRY_IGNORE_ERRORS[event_logger]:
         if re.search(error, error_message):
             return None
+    return event
