8158f5aba493695c7f1972c9cc9ce233a1b964fe
Federico Capoano <f.capoano@openwisp.io>
2025-02-21 19:37:42 -0300

    [change] Sample less requests to /api/v1/freeradius/*

--- ow_sentry_utils/__init__.py
+++ ow_sentry_utils/__init__.py
@@ -33,7 +33,7 @@ def traces_sampler(context):
     if context['transaction_context']['op'] == 'celery.task':
         return 0.0
     if context.get('asgi_scope') and context['asgi_scope']['path'].startswith(
-        ('/controller/checksum/', '/api/v1/monitoring/device/')
+        ('/controller/checksum/', '/api/v1/monitoring/device/', '/api/v1/freeradius/')
     ):
         return 0.0001
     return 0.001
